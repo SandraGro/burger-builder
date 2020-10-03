3bc46f0bceafedad66a3f1ad35a6787caa34c31a
SandraGro <sandra.guerreroflores@gmail.com>
2020-10-02 19:39:14 -0500

    Add Order Summary and Post endpoint

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
-import classes from './Layout.css'
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import classes from './Layout.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
+        this.setState( { showSideDrawer: false } );
     }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
     render () {
         return (
             <Aux>
-                <Toolbar />
-                <SideDrawer 
-                    open={this.state.showSideDrawer} 
-                    closed={this.sideDrawerClosedHandler}/>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
@@ -27,6 +34,5 @@ class Layout extends Component {
         )
     }
 }
-
 
 export default Layout;
